92f002ebaa0b09023749eb5d0cfd62da10615221
michaelpapi <michaelchijioke07@gmail.com>
2023-07-27 13:54:02 -0700

    New

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,10 +10,18 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	for (i = 0; h; i++)
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n" , h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
